57a5187db605024d391418d8e1fee1f0b8468cfd
MrFu <136260130@qq.com>
2017-04-17 11:32:54 +0800

    编码问题

--- testssm/src/main/java/com/fu/controller/FileController.java
+++ testssm/src/main/java/com/fu/controller/FileController.java
@@ -23,13 +23,13 @@ import org.springframework.web.multipart.MultipartFile;
 import com.fu.service.FileService;
 
 /**
- * 涓婁紶涓嬭浇绫�
+ * 上传下载功能
  */
 @Controller
 @RequestMapping("file")
 public class FileController {
 
-	// 娉ㄥ叆service
+	// 注入文件service
 	FileService fservice;
 
 	@Resource(name = "fservice")
@@ -38,23 +38,21 @@ public class FileController {
 	}
 
 	/**
-	 * 鏂囦欢涓婁紶鍔熻兘
+	 * 上传功能
 	 * 
 	 * @param MultipartFile
-	 *            瑕佷笂浼犵殑鏂囦欢
+	 *            获得要上传的文件
 	 * @return ajax-data
 	 * @throws IOException
 	 */
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	@ResponseBody
 	public String upload(@RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
-		// 寰楀埌涓婁紶浜�
+		
 		String fname = (String) request.getSession().getAttribute("username");
-		// 闃叉閲嶅涓婁紶,涓存椂鍚�
-		// 寰楀埌涓婁紶鏃剁殑鏂囦欢鍚�
+
 		String fileName = file.getOriginalFilename();
 
-		// 杞寲鎴�2杩涘埗
 		byte[] b = fileName.getBytes();
 
 			fservice.addFile(b, fname);
@@ -62,20 +60,20 @@ public class FileController {
 			return "upload_success";
 
 
-		//// 鑾峰彇瑕佹帴鍙椾笂浼犳枃浠剁殑鍦板潃
+
 		// String path =
 		//// request.getSession().getServletContext().getRealPath("/upload");
-		// // 寰楀埌涓婁紶鏃剁殑鏂囦欢鍚�
+
 		// String fileName = file.getOriginalFilename();
-		//// 鍓嶄竴涓弬鏁颁唬琛ㄤ笂浼犵殑鍦板潃锛屽悗闈㈢殑鍙傛暟琛ㄧず涓婂埌鍚庣殑鍚嶅瓧
+
 		// File dir = new File(path, fileName);
 		// if (!dir.exists()) {
-		// //鍒涘缓杩欎釜鍚嶅瓧
+
 		// dir.mkdirs();
 		// }
 		//
 		// try {
-		// //淇濆瓨鏂规硶
+
 		// file.transferTo(dir);
 		// return "upload_success";
 		// } catch (Exception e) {
@@ -84,28 +82,28 @@ public class FileController {
 	}
 
 	/**
-	 * 鏂囦欢涓嬭浇鍔熻兘
+	 * 下载功能
 	 */
 	@RequestMapping("/down")
 	public void down(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// 闇�瑕佷笅杞界殑鏂囦欢
+
 		String fileNameV = "";
 		if (request.getParameter("background").isEmpty()) {
 			fileNameV = request.getParameter("background");
 		} else {
 			fileNameV = request.getParameter("background");
 		}
-		// 瀹氫綅瑕佷笅杞界殑鏂囦欢
+
 		String fileName = request.getSession().getServletContext().getRealPath("upload") + "/" + fileNameV;
-		// 鑾峰彇杈撳叆娴�
+	
 		InputStream bis = new BufferedInputStream(new FileInputStream(new File(fileName)));
-		// 鍋囧浠ヤ腑鏂囧悕涓嬭浇鐨勮瘽
+	
 		String filename = fileNameV;
-		// 杞爜锛屽厤寰楁枃浠跺悕涓枃涔辩爜
+
 		filename = URLEncoder.encode(filename, "UTF-8");
-		// 璁剧疆鏂囦欢涓嬭浇澶�
+		
 		response.addHeader("Content-Disposition", "attachment;filename=" + filename);
-		// 1.璁剧疆鏂囦欢ContentType绫诲瀷锛岃繖鏍疯缃紝浼氳嚜鍔ㄥ垽鏂笅杞芥枃浠剁被鍨�
+	
 		response.setContentType("multipart/form-data");
 		BufferedOutputStream out = new BufferedOutputStream(response.getOutputStream());
 		int len = 0;
